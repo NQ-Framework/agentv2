bc0cb660b250eb491693ac9f1a9536267956bb60
Milos Spasojevic <milos.s.pfc@gmail.com>
2022-10-18 12:56:41 +0200

    default to table output. update table with edge funciton response

--- src/process-sql-query.js
+++ src/process-sql-query.js
@@ -5,7 +5,7 @@ function processSqlQuery(payload, client) {
   const status = "success";
   const errorText = undefined;
   return db.raw(payload.request.query).then((result) => {
-    if (payload.request.output === "updateTable") {
+    if (!payload.request.output || payload.request.output === "updateTable") {
       return client
         .from("agent_query")
         .update({
@@ -13,10 +13,7 @@ function processSqlQuery(payload, client) {
           executed_at: new Date(),
           executed_by: 1,
         })
-        .match({ id: payload.id })
-        .then((p) => {
-          console.log("done.", p);
-        });
+        .match({ id: payload.id });
     }
     if (payload.request.output === "callUrl") {
       const { url } = payload.request;
@@ -29,23 +26,17 @@ function processSqlQuery(payload, client) {
           : { "Content-Type": "application/json" },
         body,
       })
-        .then((res) => {
-          console.log("res je", res);
-          return res.text();
-        })
+        .then((res) => res.text())
         .then((text) => {
-          console.log("called api and got result: ", text);
+          console.log("api result: ", text);
           client
             .from("agent_query")
             .update({
-              result: { status, errorText, resultSet: null },
+              result: { status, errorText, apiResponse: text },
               executed_at: new Date(),
               executed_by: 1,
             })
-            .match({ id: payload.id })
-            .then((p) => {
-              console.log("done.", p);
-            });
+            .match({ id: payload.id });
         });
     }
     throw new Error("invalid output type");
